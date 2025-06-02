98fb885ad53f82a63918ba9606cb0f5c87ababc4
Shawn Sun <shawnsun928@outlook.com>
2025-06-02 18:42:16 -0400

    Update __init__.py

--- TECHNIC/__init__.py
+++ TECHNIC/__init__.py
@@ -24,13 +24,14 @@ from .internal import *
 from .mev import *
 from .data import *
 from .model import *
+from .feature import *
 from .transform import *
 from .cm import *
 from .plot import*
 from .report import *
 from .segment import *
-from .writer import *
-from .template import *
+# from .writer import *
+# from .template import *
 from .test import *
 from .condition import *
 from .scenario import *
